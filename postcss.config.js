c46a81eef72087f2233df8e8037c7664b967e0a3
Abhishek singh chauhan <avi7000201383@gmail.com>
2023-11-20 00:41:54 +0530

    post css error fix

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    "postcss-nested": {}, // Add this line for CSS nesting
+    // Add other PostCSS plugins as needed
+  },
+};
