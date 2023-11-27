b5dc1a852e5acccdb2e20a39e77f5b7d7c45518c
Abhishek singh chauhan <avi7000201383@gmail.com>
2023-11-27 11:56:59 +0530

    added year calender

---
+++ src/components/home-services/application/Calender2.jsx
@@ -0,0 +1,352 @@
+import React, { useState } from "react";
+import { Fragment } from "react";
+import {
+  FaChevronDown,
+  FaChevronLeft,
+  FaChevronRight,
+  FaEllipsisH,
+} from "react-icons/fa";
+import { Menu, Transition } from "@headlessui/react";
+
+const generateMonthss = () => {
+  const currentDate = new Date();
+  const next12Months = Array.from({ length: 12 }, (_, index) => {
+    const firstDayOfMonth = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth() + index,
+      1
+    );
+    const daysInMonth = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth() + index + 1,
+      0
+    ).getDate();
+
+    const days = Array.from({ length: daysInMonth }, (_, dayIndex) => {
+      const day = new Date(firstDayOfMonth);
+      day.setDate(dayIndex + 1);
+      const dateString = day.toISOString().split("T")[0];
+
+      return {
+        date: dateString,
+        isCurrentMonth: index === 0,
+        isToday: dateString === currentDate.toISOString().split("T")[0],
+      };
+    });
+
+    return {
+      name: firstDayOfMonth.toLocaleString("default", { month: "long" }),
+      days,
+    };
+  });
+
+  return next12Months;
+};
+
+const months = generateMonthss();
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const Calendar2 = () => {
+  return (
+    <div>
+      <header className="flex items-center justify-between border-b border-gray-200 px-6 py-4">
+        <h1 className="text-base font-semibold leading-6 text-gray-900">
+          <time dateTime="2022">2022</time>
+        </h1>
+        <div className="flex items-center">
+          <div className="relative flex items-center rounded-md bg-white shadow-sm md:items-stretch">
+            <div
+              className="pointer-events-none absolute inset-0 rounded-md ring-1 ring-inset ring-gray-300"
+              aria-hidden="true"
+            />
+            <button
+              type="button"
+              className="flex items-center justify-center rounded-l-md py-2 pl-3 pr-4 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:px-2 md:hover:bg-gray-50"
+            >
+              <span className="sr-only">Previous year</span>
+              <FaChevronLeft className="h-5 w-5" aria-hidden="true" />
+            </button>
+            <button
+              type="button"
+              className="hidden px-3.5 text-sm font-semibold text-gray-900 hover:bg-gray-50 focus:relative md:block"
+            >
+              Today
+            </button>
+            <span className="relative -mx-px h-5 w-px bg-gray-300 md:hidden" />
+            <button
+              type="button"
+              className="flex items-center justify-center rounded-r-md py-2 pl-4 pr-3 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:px-2 md:hover:bg-gray-50"
+            >
+              <span className="sr-only">Next year</span>
+              <FaChevronRight className="h-5 w-5" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="hidden md:ml-4 md:flex md:items-center">
+            <Menu as="div" className="relative">
+              <Menu.Button
+                type="button"
+                className="flex items-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+              >
+                Year view
+                <FaChevronDown
+                  className="-mr-1 h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </Menu.Button>
+
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 z-10 mt-3 w-36 origin-top-right overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <div className="py-1">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          href="#"
+                          className={classNames(
+                            active
+                              ? "bg-gray-100 text-gray-900"
+                              : "text-gray-700",
+                            "block px-4 py-2 text-sm"
+                          )}
+                        >
+                          Day view
+                        </a>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          href="#"
+                          className={classNames(
+                            active
+                              ? "bg-gray-100 text-gray-900"
+                              : "text-gray-700",
+                            "block px-4 py-2 text-sm"
+                          )}
+                        >
+                          Week view
+                        </a>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          href="#"
+                          className={classNames(
+                            active
+                              ? "bg-gray-100 text-gray-900"
+                              : "text-gray-700",
+                            "block px-4 py-2 text-sm"
+                          )}
+                        >
+                          Month view
+                        </a>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          href="#"
+                          className={classNames(
+                            active
+                              ? "bg-gray-100 text-gray-900"
+                              : "text-gray-700",
+                            "block px-4 py-2 text-sm"
+                          )}
+                        >
+                          Year view
+                        </a>
+                      )}
+                    </Menu.Item>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+            <div className="ml-6 h-6 w-px bg-gray-300" />
+            <button
+              type="button"
+              className="ml-6 rounded-md  bg-pink-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600"
+            >
+              Add event
+            </button>
+          </div>
+          <Menu as="div" className="relative ml-6 md:hidden">
+            <Menu.Button className="-mx-2 flex items-center rounded-full border border-transparent p-2 text-gray-400 hover:text-gray-500">
+              <span className="sr-only">Open menu</span>
+              <FaEllipsisH className="h-5 w-5" aria-hidden="true" />
+            </Menu.Button>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 z-10 mt-3 w-36 origin-top-right divide-y divide-gray-100 overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="py-1">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#"
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Create event
+                      </a>
+                    )}
+                  </Menu.Item>
+                </div>
+                <div className="py-1">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#"
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Go to today
+                      </a>
+                    )}
+                  </Menu.Item>
+                </div>
+                <div className="py-1">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#"
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Day view
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#"
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Week view
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#"
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Month view
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#"
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Year view
+                      </a>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </div>
+      </header>
+      <div className="bg-white">
+        <div className="mx-auto grid max-w-3xl grid-cols-1 gap-x-8 gap-y-16 px-4 py-16 sm:grid-cols-2 sm:px-6 xl:max-w-none xl:grid-cols-3 xl:px-8 2xl:grid-cols-4">
+          {months.map((month) => (
+            <section key={month.name} className="text-center">
+              <h2 className="text-sm font-semibold text-gray-900">
+                {month.name}
+              </h2>
+              <div className="mt-6 grid grid-cols-7 text-xs leading-6 text-gray-500">
+                <div>M</div>
+                <div>T</div>
+                <div>W</div>
+                <div>T</div>
+                <div>F</div>
+                <div>S</div>
+                <div>S</div>
+              </div>
+              <div className="isolate mt-2 grid grid-cols-7 gap-px rounded-lg bg-gray-200 text-sm shadow ring-1 ring-gray-200">
+                {month.days.map((day, dayIdx) => (
+                  <button
+                    key={day.date}
+                    type="button"
+                    className={classNames(
+                      day.isCurrentMonth
+                        ? "bg-white text-gray-900"
+                        : "bg-gray-50 text-gray-400",
+                      dayIdx === 0 && "rounded-tl-lg",
+                      dayIdx === 6 && "rounded-tr-lg",
+                      dayIdx === month.days.length - 7 && "rounded-bl-lg",
+                      dayIdx === month.days.length - 1 && "rounded-br-lg",
+                      "py-1.5 hover:bg-gray-100 focus:z-10"
+                    )}
+                  >
+                    <time
+                      dateTime={day.date}
+                      className={classNames(
+                        day.isToday && "bg-pink-600 font-semibold text-white",
+                        "mx-auto flex h-7 w-7 items-center justify-center rounded-full"
+                      )}
+                    >
+                      {day.date.split("-").pop().replace(/^0/, "")}
+                    </time>
+                  </button>
+                ))}
+              </div>
+            </section>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Calendar2;
--- src/subContainer/calender/Calender.jsx
+++ src/subContainer/calender/Calender.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 
 import Calender1 from "../../components/home-services/application/Calender1";
+import Calender2 from "../../components/home-services/application/Calender2";
 
 import ReactCalender1 from "!!raw-loader!../../components/home-services/application/Calender1";
+import ReactCalender2 from "!!raw-loader!../../components/home-services/application/Calender2";
 
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { darcula } from "react-syntax-highlighter/dist/esm/styles/prism";
@@ -14,6 +16,11 @@ const ComponentData = [
     id: 1,
     component: Calender1,
     rawString_React: ReactCalender1,
+  },
+  {
+    id: 2,
+    component: Calender2,
+    rawString_React: ReactCalender2,
   },
 ];
 
